bc2edd417e66e16d45d4d007f103a717a70bda0a
Gilson Sales <41352169+gsphpsales@users.noreply.github.com>
2018-11-01 18:16:44 -0300

    Main activity teste
    criação das variaveis e vinculação dos id's

--- app/src/main/java/com/developer/gilsonsales/mediaescolar/MainActivity.java
+++ app/src/main/java/com/developer/gilsonsales/mediaescolar/MainActivity.java
@@ -2,12 +2,21 @@ package com.developer.gilsonsales.mediaescolar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imagem;
+    TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagem = findViewById(R.id.seta);
+        texto = findViewById(R.id.texto);
+        texto.setText("Toque aqui para continuar");
+        imagem.setScaleX(-1f);
+
     }
 }
